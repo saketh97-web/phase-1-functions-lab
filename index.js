6504350539c5906b4c4bf0ded592973400d3529f
Saketh <sakethkamb@gmail.com>
2023-04-10 10:04:05 -0700

    commit tree 10d1a0a1ab4029d4ee1ff0b2174b38e9c5f44428

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+    function distanceFromHqInBlocks(someValue) {
+    //returns the number of blocks given a value
+   
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    distanceFromHqInBlocks(someValue);
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+      //returns the number of feet traveled
+    }
+  
+    function calculatesFarePrice(start, destination) {
+      //returns the fare for the customer
+    }
